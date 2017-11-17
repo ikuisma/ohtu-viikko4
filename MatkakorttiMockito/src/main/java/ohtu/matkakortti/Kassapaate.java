2bcf220b03b661cfa04d4e9ad709c1d00d9b6428
ikuisma <ilkka.a.kuisma@gmail.com>
2017-11-18 00:29:33 +0200

    Fixed ostaLounas method.

--- MatkakorttiMockito/src/main/java/ohtu/matkakortti/Kassapaate.java
+++ MatkakorttiMockito/src/main/java/ohtu/matkakortti/Kassapaate.java
@@ -13,8 +13,10 @@ public class Kassapaate {
     }
     
     public void ostaLounas(Matkakortti kortti) {
-        kortti.osta(HINTA);
-        myytyjaLounaita++;
+        if (kortti.getSaldo() >= HINTA) {
+            kortti.osta(HINTA);
+            myytyjaLounaita++;
+        }
     }
 
     public int getMyytyjaLounaita() {
