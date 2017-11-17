c040d084623f04c09295195afe347a05c0b042c5
ikuisma <ilkka.a.kuisma@gmail.com>
2017-11-18 00:41:58 +0200

    Added tests for loading money to the card.

--- MatkakorttiMockito/src/test/java/ohtu/lyyrakortti/KassapaateTest.java
+++ MatkakorttiMockito/src/test/java/ohtu/lyyrakortti/KassapaateTest.java
@@ -40,4 +40,18 @@ public class KassapaateTest {
         verify(kortti, times(1)).getSaldo();
         verify(kortti, times(0)).osta(anyInt());
     }
+
+    @Test
+    public void kortilleLadataanRahaaJosMaaraOnPositiivinen() {
+        when(kortti.getSaldo()).thenReturn(4);
+        kassa.lataa(kortti, 10);
+        verify(kortti, times(1)).lataa(anyInt());
+    }
+
+    @Test
+    public void kortilleEiLadataRahaaJosMaaraOnNegatiivinen() {
+        kassa.lataa(kortti, -10);
+        verify(kortti, times(0)).lataa(anyInt());
+    }
+
 }
