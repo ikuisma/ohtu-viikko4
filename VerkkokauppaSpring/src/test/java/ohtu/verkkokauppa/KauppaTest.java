3457cccba68f06d0b61e118adc1accb3ad2a40d2
ikuisma <ilkka.a.kuisma@gmail.com>
2017-11-18 01:45:53 +0200

    Added first Mockito test for Kauppa class.

---
+++ VerkkokauppaSpring/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -0,0 +1,42 @@
+package ohtu.verkkokauppa;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+public class KauppaTest {
+
+    Kauppa kauppa;
+    Varasto varasto;
+    Pankki pankki;
+    Viitegeneraattori viitegeneraattori;
+
+    @Before
+    public void setup() {
+        varasto = mock(Varasto.class);
+        pankki = mock(Pankki.class);
+        viitegeneraattori = mock(Viitegeneraattori.class);
+        kauppa = new Kauppa(varasto, pankki, viitegeneraattori);
+    }
+
+    @Test
+    public void ostoksenPaaytyttyaPankinMetodiaTilisiirtoKutsutaan() {
+        when(viitegeneraattori.uusi()).thenReturn(1);
+
+        when(varasto.saldo(1)).thenReturn(100);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "Koff Portteri", 3));
+
+        kauppa.aloitaAsiointi();
+        kauppa.lisaaKoriin(1);
+        verify(varasto, times(1)).haeTuote(1);
+
+        kauppa.tilimaksu("pekka", "12345");
+
+        // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
+        verify(pankki).tilisiirto("pekka", 1, "12345", "33333-44455", 3);
+    }
+
+}
